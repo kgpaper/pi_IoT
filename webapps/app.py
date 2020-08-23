1c647563698feb969b6fef811d24e70afad434eb
sanha <kgpaper@naver.com>
2020-08-24 03:10:10 +0900

    2020/08/24

--- webapps/app.py
+++ webapps/app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/")
 def test():
-    return render_template("test.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
